bc4eb785dee25cfbd146224534b42f382f1ce7a4
jimin <thesuffering@naver.com>
2018-10-09 12:23:06 +0900

    서지민 작업 - 2
    클라우드 스택 설치가 빡처서 그냥 아예 클라우드 서버 자체를 만듬

---
+++ bash/클라우드 스택 설치2.bash
@@ -0,0 +1,25 @@
+sudo apt-get update
+sudo apt-get install tasksel
+sudo tasksel install lamp-server
+//실패
+
+//Apache2 서버 이용하기
+apt-get update
+apt-get upgrade
+apt-get install apache2
+http://127.0.1.1
+
+//PHP 설치
+apt-get install php
+apt-get install php-cli
+
+//Mysql 설치
+apt-get install php-mysql 
+apt-get install mysql-client
+apt-get install mysql-server
+
+//사이트 열기
+a2ensite
+/etc/init.d/apache2 reload
+
+//성공
