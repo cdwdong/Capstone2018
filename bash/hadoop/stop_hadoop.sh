b7b7f8bdd1882e5eaef6ac1d0d57414f3bccdbf1
capproject <cdwdong1@gmail.com>
2018-10-22 21:44:11 +0900

    bash update

--- bash/hadoop/stop_hadoop.sh
+++ bash/hadoop/stop_hadoop.sh
@@ -1,6 +1,7 @@
-$HADOOP_HOME/sbin/stop-all.sh
-for number in 10 11 12 13
-do
-	ssh worker-$number "rm -rf /opt/hadoop_tmp/hdfs/datanode/*"
-done
+#$HADOOP_HOME/sbin/stop-all.sh
+while read h; do
+	if [ -z "$h" ]; then continue; fi
+	hn=`echo $h | cut -d ' ' -f2`
+	ssh $hn "rm -rf /opt/hadoop_tmp/hdfs/datanode/*"
+done < /etc/hosts
 $HADOOP_HOME/bin/hdfs namenode -format
